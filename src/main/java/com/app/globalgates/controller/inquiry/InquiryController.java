1eba580ca2234c392782f9f55a1b48e2e2d6513e
ljs528022 <ljs528022@gmail.com>
2026-03-25 11:08:33 +0900

    inquiry list mapper 작성 중 #60

---
+++ src/main/java/com/app/globalgates/controller/inquiry/InquiryController.java
@@ -0,0 +1,14 @@
+package com.app.globalgates.controller.inquiry;
+
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/inquiry/**")
+@RequiredArgsConstructor
+@Slf4j
+public class InquiryController {
+}
